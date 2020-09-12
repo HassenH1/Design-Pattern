694d96dda7a3b9818b74fd7ec61b24f872cd0a4f
HassenH1 <hasansaid51@gmail.com>
2020-09-11 18:41:01 -0700

    implementation

--- Facade pattern/facadePattern.js
+++ Facade pattern/facadePattern.js
@@ -1,1 +1,21 @@
 //The facade pattern hides complex code with easier to use higher level code example jQuery, AJAX
+
+function addEvent(elem, event, func) {
+  if (elem.addEventListener) {
+    elem.addEventListener(event, func, false);
+  } else if (elem.attachEvent) {
+    elem.attachEvent("on" + event, func);
+  } else {
+    elem["on" + event] = func;
+  }
+}
+
+function removeEvent(elem, event, func) {
+  if (elem.removeEventListener) {
+    elem.removeEventListener(event, func, false);
+  } else if (elem.detachEvent) {
+    elem.detachEvent("on" + event, func);
+  } else {
+    elem["on" + event] = null;
+  }
+}
