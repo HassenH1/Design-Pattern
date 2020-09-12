d371a8cde95ddc4755c90667b323e5306f60aee1
HassenH1 <hasansaid51@gmail.com>
2020-09-11 18:55:27 -0700

    finished facade pattern

--- Facade pattern/facadePattern.js
+++ Facade pattern/facadePattern.js
@@ -19,3 +19,70 @@ function removeEvent(elem, event, func) {
     elem["on" + event] = null;
   }
 }
+
+//////////another example
+
+class Shape {
+  draw() {}
+}
+
+class Rectangle extends Shape {
+  //override parent method
+  draw() {
+    super.draw();
+    console.log("Rectangle::draw()");
+  }
+}
+
+class Square extends Shape {
+  draw() {
+    super.draw();
+    console.log("Square::draw()");
+  }
+}
+
+class Circle extends Shape {
+  draw() {
+    super.draw();
+    console.log("Circle::draw()");
+  }
+}
+
+class ShapeMaker {
+  constructor() {
+    this.circle = new Circle();
+    this.rectangle = new Rectangle();
+    this.square = new Square();
+  }
+
+  drawCircle() {
+    this.circle.draw();
+  }
+
+  drawRectangle() {
+    this.rectangle.draw();
+  }
+
+  drawSquare() {
+    this.square.draw();
+  }
+}
+
+class FacadePatternDemo {
+  main() {
+    let shapeMaker = new ShapeMaker();
+
+    shapeMaker.drawCircle();
+    shapeMaker.drawRectangle();
+    shapeMaker.drawSquare();
+  }
+}
+
+let FPD = new FacadePatternDemo();
+FPD.main();
+
+/***Output
+Circle::draw()
+Rectangle::draw()
+Square::draw()
+***/
